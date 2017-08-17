1c1dcf35a55079e3d9bf8b945f6e6df0caa83d8c
Andrew <expandrew@gmail.com>
2017-08-17 14:33:26 -0700

    work on using a mock database in our tests.

--- ToDoList.Tests/ItemsControllerTest.cs
+++ ToDoList.Tests/ItemsControllerTest.cs
@@ -1,19 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Controllers;
 using ToDoList.Models;
 using Xunit;
+using System.Linq;
+using Moq;
 
 namespace ToDoList.Tests
 {
     public class ItemsControllerTest
     {
+        Mock<IItemRepository> mock = new Mock<IItemRepository>();
+
+        private void DbSetup()
+        {
+            mock.Setup(m => m.Items).Returns(new Item[]
+            {
+                new Item {ItemId = 1, Description = "Wash the dog" },
+                new Item {ItemId = 2, Description = "Do the dishes" },
+                new Item {ItemId = 3, Description = "Sweep the floor" }
+            }.AsQueryable());
+        }
+
         [Fact]
-        public void Get_ViewResult_Index_Test()
+        public void Mock_GetViewResultIndex_Test() //Confirms route returns view
         {
             //Arrange
-            ItemsController controller = new ItemsController();
+            DbSetup();
+            ItemsController controller = new ItemsController(mock.Object);
 
             //Act
             var result = controller.Index();
@@ -21,29 +35,33 @@ namespace ToDoList.Tests
             //Assert
             Assert.IsType<ViewResult>(result);
         }
-        [Fact]
-        public void Get_ModelList_Index_Test()
-        {
-            //Arrange
-            ViewResult indexView = new ItemsController().Index() as ViewResult;
 
-            //Act
-            var result = indexView.ViewData.Model;
-
-            //Assert
-            Assert.IsType<List<Item>>(result);
-        }
         [Fact]
-        public void Post_MethodAddsItem_Test()
+        public void Mock_IndexListOfItems_Test() //Confirms model as list of items
         {
             // Arrange
-            ItemsController controller = new ItemsController();
-            Item testItem = new Item();
-            testItem.Description = "test item";
+            DbSetup();
+            ViewResult indexView = new ItemsController(mock.Object).Index() as ViewResult;
 
             // Act
-            controller.Create(testItem);
-            ViewResult indexView = new ItemsController().Index() as ViewResult;
+            var result = indexView.ViewData.Model;
+
+            // Assert
+            Assert.IsType<List<Item>>(result);
+        }
+
+        [Fact]
+        public void Mock_ConfirmEntry_Test() //Confirms presence of known entry
+        {
+            // Arrange
+            DbSetup();
+            ItemsController controller = new ItemsController(mock.Object);
+            Item testItem = new Item();
+            testItem.Description = "Wash the dog";
+            testItem.ItemId = 1;
+
+            // Act
+            ViewResult indexView = controller.Index() as ViewResult;
             var collection = indexView.ViewData.Model as IEnumerable<Item>;
 
             // Assert
--- src/ToDoList/Controllers/ItemsController.cs
+++ src/ToDoList/Controllers/ItemsController.cs
@@ -1,19 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using ToDoList.Models;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ToDoList.Controllers
 {
     public class ItemsController : Controller
     {
-        private ToDoListContext db = new ToDoListContext();
-        public IActionResult Index()
+        private IItemRepository itemRepo;
+
+        public ItemsController(IItemRepository thisRepo = null)
         {
-            return View(db.Items.ToList());
+            if (thisRepo == null)
+            {
+                this.itemRepo = new EFItemRepository();
+            }
+            else
+            {
+                this.itemRepo = thisRepo;
+            }
         }
+
+
+        public ViewResult Index()
+        {
+            return View(itemRepo.Items.ToList());
+        }
+
         public IActionResult Details(int id)
         {
-            var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+            Item thisItem = itemRepo.Items.FirstOrDefault(x => x.ItemId == id);
             return View(thisItem);
         }
         public IActionResult Create()
@@ -24,8 +43,33 @@ namespace ToDoList.Controllers
         [HttpPost]
         public IActionResult Create(Item item)
         {
-            db.Items.Add(item);
-            db.SaveChanges();
+            itemRepo.Save(item);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Item thisItem = itemRepo.Items.FirstOrDefault(x => x.ItemId == id);
+            return View(thisItem);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Item item)
+        {
+            itemRepo.Edit(item);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            Item thisItem = itemRepo.Items.FirstOrDefault(x => x.ItemId == id);
+            return View(thisItem);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            Item thisItem = itemRepo.Items.FirstOrDefault(x => x.ItemId == id);
+            itemRepo.Remove(thisItem);
             return RedirectToAction("Index");
         }
     }
